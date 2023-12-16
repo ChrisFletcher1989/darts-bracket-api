1d87ccbe1c661924ae8534f13a43f2dfd824d757
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-12-16 10:19:38 +0900

    added bracketPlayers handler in addBracket component

---
+++ controllers/addBracket.js
@@ -0,0 +1,50 @@
+//TO DO: Add players and scores to bracketPlayersDB
+
+const handleAddBracket = (req, res, db, bcrypt) => {
+  const { email, passcode, password } = req.body;
+  if (!email || !passcode || !password) {
+    return res.status(400).json("incorrect bracket login data");
+  }
+  bcrypt.hash(password, null, null, function (err, hash) {});
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+        passcode: passcode,
+      })
+      .into("bracketCredentials")
+      .returning("passcode")
+      .then((loginEmail) => {
+        return trx("bracketPlayers")
+          .returning("*")
+          .insert({
+            email: loginEmail[0].email,
+            passcode: passcode,
+            created: new Date(),
+            firstPlayers: firstPlayers,
+            secondPlayers: secondPlayers,
+            thirdPlayers: thirdPlayers,
+            fourthPlayers: fourthPlayers,
+            fifthPlayers: fifthPlayers,
+            sixthPlayers: sixthPlayers,
+            firstScores: firstScores,
+            secondScores: secondScores,
+            thirdScores: thirdScores,
+            fourthScores: fourthScores,
+            fifthScores: fifthScores,
+            sixthScores: sixthScores,
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
+};
+
+module.exports = {
+  handleAddBracket: handleAddBracket,
+};
